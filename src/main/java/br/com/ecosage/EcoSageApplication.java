0d7d0ce00bbaf454dbc72f167136da597de88183
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-11-12 09:59:45 -0300

    building project

---
+++ src/main/java/br/com/ecosage/EcoSageApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecosage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ecosage/EcoSageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecosage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
